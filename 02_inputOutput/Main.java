e2e347b12167c4032e417e0f502b6250e271badc
rohanmaiti <maitirohan2019@gmail.com>
2024-09-05 00:31:32 +0530

    input output in java

---
+++ 02_inputOutput/Main.java
@@ -0,0 +1,6 @@
+/**
+ *Difference between println(), print() and printf()
+  print() - It prints string inside the quotes.
+  println() - It prints string inside the quotes similar like print() method. Then the cursor moves to the beginning of the next line.
+  printf() - It provides string formatting
+ */
