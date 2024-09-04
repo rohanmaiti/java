9844d5ce2df0a7eaebc473edddcc1f5e98266d65
rohanmaiti <maitirohan2019@gmail.com>
2024-09-05 00:30:23 +0530

    introduction of java

---
+++ 01_Introduction/Main.java
@@ -0,0 +1,31 @@
+public class Main{
+    public static void main(String []args){
+        System.out.println("Hello Rohan");
+        // datatypes in java
+        /**
+         * 1. premitive (8) || boolean , byte ,short , int , long , char , float , double
+         * 2. non premitive
+         */
+        	
+    // long range = -42332200000l;
+    // System.out.println(range); 
+    /**Notice, the use of L at the end of -42332200000. This represents that it's an integer of the long type. */
+
+    // double number = -42.3;
+    // System.out.println(number); // prints -42.3
+
+
+    //  float number = -42.3f;
+    // System.out.println(number);  // prints -42.3
+    /**Notice that we have used -42.3f instead of -42.3in the above program. It's because -42.3 is a double literal. */
+
+    /*The minimum value of the char data type is '\u0000' (0) and the maximum value of the is '\uffff'.
+    Default value: '\u0000'*/
+    // char letter = '\u0051';
+    // System.out.println(letter);  // prints Q
+
+
+    
+
+    }
+}
