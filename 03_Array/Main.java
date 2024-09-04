f77f0346ff38b8888c1a46cb00bd43e20ca280b3
rohanmaiti <maitirohan2019@gmail.com>
2024-09-05 00:32:07 +0530

    Java Array

---
+++ 03_Array/Main.java
@@ -0,0 +1,31 @@
+public class Main{
+    public static void main(String []args){
+       // declare a array 
+       int arr[];
+       // define size
+       arr = new int[10];
+     
+
+        //declare and initialize and array
+        int[] age = {12, 4, 5, 2, 5};
+
+        // declare and initialise it using new 
+        int roll[] = new int[]{1,2,3,4};
+
+        // multidimentional array 
+        double[][] matrix = {{1.2, 4.3, 4.0},{4.1, -1.1},{1,2}};
+
+
+        /*********** ******************** ******************  */
+        // copying an array  unsing assign operator
+         int [] numbers = {1, 2, 3, 4, 5, 6};
+         System.out.print("hii"+" : "+numbers);
+        int [] positiveNumbers = numbers;    // copying arrays
+
+        // for (int number: positiveNumbers) {
+        //     System.out.print(number + ", ");
+        // }
+        // System.out.println(); 
+ 
+    }
+}
