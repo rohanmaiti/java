dd0f12c381cc22814ebe32a940a70dada0ad8dd5
rohanmaiti <maitirohan2019@gmail.com>
2024-09-06 12:46:44 +0530

    Inheritance

---
+++ 09_inheritance/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String []args){
+        // System.out.println("Hello Rohan");\
+        // Calc obj = new Calc();
+        // System.out.println(obj.add(10,20));
+        // System.out.println(obj.sub(20,10));
+
+        // AdvCalc obj = new AdvCalc();
+        // System.out.println(obj.add(10,20)); 
+        // System.out.println(obj.mul(10,20)); 
+    }
+}
