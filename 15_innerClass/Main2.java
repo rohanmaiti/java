cfe3f077fb42a6a1414a9ba10947216f0c7560dd
rohanmaiti <maitirohan2019@gmail.com>
2024-09-07 22:18:32 +0530

    .\15_innerClass\

---
+++ 15_innerClass/Main2.java
@@ -0,0 +1,38 @@
+// public class Main2{
+//     public static void main(String []args){
+//         A obj = new B();
+//         obj.print();
+//     }
+// }
+// class A{
+//     void print(){
+//         System.out.println("Print A");
+//     }
+// }
+// class B extends A{
+//     void print(){
+//         System.out.println("Print B");
+//     }
+// }
+
+
+// just to change the behavoiur of A we are defining another class .
+// now what we can do is like this in below 
+
+public class Main2{
+    public static void main(String []args){
+        A obj = new A()
+        {
+            void print(){
+                System.out.println("Print B");
+            }
+        };
+        obj.print();
+    }
+}
+class A{
+    void print(){
+        System.out.println("Print A");
+    }
+}
+
