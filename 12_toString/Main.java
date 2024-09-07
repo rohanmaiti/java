251e0482b47d6e130b44be64dd6b788b7609da1e
rohanmaiti <maitirohan2019@gmail.com>
2024-09-07 22:15:49 +0530

    toString

---
+++ 12_toString/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String []args){
+    A obj = new A();
+    System.out.println(obj);
+    }
+}
+class A{
+    public String toString(){
+     return "This is A obj";
+    }
+}
