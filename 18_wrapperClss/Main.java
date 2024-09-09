7f35568021b3b3cd84e915ebcc9bba35ab8ff790
rohanmaiti <maitirohan2019@gmail.com>
2024-09-09 22:21:50 +0530

    Wrapper class

---
+++ 18_wrapperClss/Main.java
@@ -0,0 +1,24 @@
+public class Main{
+    public static void main(String args[]){
+        int  n = 10;
+        Integer n2 = 10;
+        // Integer num = Integer.valueOf(n);   // auto boxing || premitive --> object
+        // Integer num = Integer.valueOf(10); //parameter:  number
+        // Integer num = Integer.valueOf("1234"); // parameter : number String
+        // Integer num = Integer.valueOf("abc"); //  give exception 
+
+        // String s = num.toString();
+       
+        // System.out.println(s);
+        // System.out.println();
+
+        // System.out.println(n == n2); // true;
+
+        // int n3 = n2.intValue();
+        // System.out.println(n3);
+
+        // System.out.println(Integer.parseInt("20"));
+        // System.out.println(Float.parseFloat("20"));
+        // System.out.println(Double.parseDouble("20"));
+    }
+}
---
+++ 18_wrapperClss/Main2.java
@@ -0,0 +1,72 @@
+public class Main2{
+    public static void main(String []args){
+        // Intiger class 
+        // 1. Autoboxing 
+        // int a = 10;
+        // Integer i1 = a;
+        // Integer i2 = 20;
+        // Integer i3 = Integer.valueOf(10);
+        // Integer i3 = Integer.valueOf(a);
+        // Integer i3 = Integer.valueOf("30");
+
+        // int num = Integer.parseInt("123"); // parameter --> only String
+        // System.out.println(num);
+
+        // int x = 101020;
+        // String s = Integer.toString(x);
+        // System.out.println(s);
+        // Integer a = 100;
+        // System.out.println(a.toString());
+
+        // Integer num1 = 100;
+        // Integer num2 = 200;
+        // int result = num1.compareTo(num2); 
+        // System.out.println(result);
+
+
+        // Integer num1 = 100;
+        // Integer num2 = 100;
+        // boolean isEqual = num1.equals(100);  // true
+        // boolean isEqual = num1.equals("100"); 
+        // System.out.println(isEqual);
+
+        // unboxing
+        // Integer num = 123;
+        // int i = num.intValue();
+        // int i = num;
+        // System.out.print(i);
+
+        // int n1 =10;
+        // int n2 =20;
+        // int result = Integer.min(n1,n2);
+        // System.out.println(result);
+
+        // System.out.println(Integer.MAX_VALUE);
+        // System.out.println(Integer.MIN_VALUE);
+
+
+        // FLOAT
+        // Float f = 10.3f;
+        // Float f2 = Float.valueOf(13.4f);
+
+        // float num = Float.parseFloat("123.3737");
+        // System.out.println(num);
+        // float f = Float.parseFloat(34.56); // error
+
+        // ** Character
+        // char c = 'a';
+        // char x = '9';
+        // System.out.println(Character.toUpperCase(c));
+        // System.out.println(Character.isDigit(c));
+        // System.out.println(Character.isDigit(x));
+        // System.out.println(Character.isLetter(c));
+        // System.out.println(Character.isLetter(x));
+        // System.out.println(Character.isLetterOrDigit('A'));
+
+        // Character d = 'd';
+        // int y = d.charValue();
+
+
+
+    }
+}
