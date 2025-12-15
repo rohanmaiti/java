8384c5bf04b8f18c2652fd6507c28c6dc5ab4711
rohanmaiti <maitirohan2019@gmail.com>
2025-12-15 23:59:59 +0530

    chore: refactor

--- 17_collection/theory.java
+++ 17_collection/theory.java
@@ -1,3 +1,6 @@
-// * Java Collection Framework (JCF) is a set of classes and interfaces that provide ready-made data structures to store and manipulate groups of objects efficiently.
+// * Java Collection Framework (JCF) is a set of classes and interfaces that
+// provide ready-made data structures to store and manipulate groups of objects
+// efficiently.
 
-// * In Java, the Collections Framework is a hierarchy of interfaces and classes that provides easy management of a group of objects.
+// * In Java, the Collections Framework is a hierarchy of interfaces and classes
+// that provides easy management of a group of objects.
